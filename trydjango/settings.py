e4d6c05baa474b21d15fe3c75333269e88e5d2a5
sierrahornet54 <hartmj54@gmail.com>
2024-11-14 16:35:30 -0800

    Update settings.py

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -26,7 +26,7 @@ DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
 
 ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
 ALLOWED_HOSTS = []
-ALLOWED_HOSTS += [".ondigitalocean.app",]
+#ALLOWED_HOSTS += [".ondigitalocean.app",]
 if not DEBUG:
     ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
 
